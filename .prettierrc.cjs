b62a1034d865b89a0427a4d59230747c578afc18
jonz94 <jody16888@gmail.com>
2022-04-09 15:05:05 +0800

    🎉 initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+
+  plugins: [
+    require('prettier-plugin-packagejson'),
+    require('./.prettier-plugins-workaround.cjs'),
+  ],
+}
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="grid h-screen w-screen place-content-center">
+    <div>Hello, world!</div>
+  </main>
+</template>
